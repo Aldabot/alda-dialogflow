bdcbfb11710fddca1eb917766a88bdea64d22862
Dirk Hornung <dirkhornung91@gmail.com>
2018-10-17 15:46:57 +0200

    refactored to Class

--- index.js
+++ index.js
@@ -1,44 +1,27 @@
-'use strict';
 const dialogflow = require('dialogflow');
 const sessionClient = new dialogflow.SessionsClient();
 
-const projectId = 'alda-57116';
+class AldaDialogflow {
+  constructor(projectId) {
+    this.projectId = projectId;
+  }
 
-const fulfillment = async (event, context) => {
-  const { query, sessionId } = JSON.parse(event.body)
-  if(!query || !sessionId)
-    return {
-      statusCode: 500,
-      body: 'query or sessionId not set'
-    }
-  const languageCode = 'es';
+  fulfillment(query, sessionId) {
+    const languageCode = 'es';
+    const sessionPath = sessionClient.sessionPath(this.projectId, sessionId);
 
-  const sessionPath = sessionClient.sessionPath(projectId, sessionId);
-
-  const request = {
-    session: sessionPath,
-    queryInput: {
-      text: {
-        text: query,
-        languageCode: languageCode,
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          text: query,
+          languageCode: languageCode,
+        },
       },
-    },
-  };
-
-  try {
-    const responses = await sessionClient.detectIntent(request)
-    const fulfillmentMessages = responses[0].queryResult.fulfillmentMessages
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ fulfillmentMessages })
-    }
-  }
-  catch(err) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify(err),
     };
-  }
-};
 
-module.exports = fulfillment
+    return sessionClient.detectIntent(request)
+      .then(res => res[0].queryResult.fulfillmentMessages)
+  };
+}
+module.exports = AldaDialogflow
